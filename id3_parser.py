5c5077597b40b6029d969683966f8bed23dcc0df
Alex Addy <alex.addy@gmail.com>
2012-10-06 18:22:11 -0400

    Updated files.
    Added a gitignore file, so that git doens't try to upload any mp3s we
    have in the directory. Also worked some on the main file.

--- id3_parser.py
+++ id3_parser.py
@@ -2,6 +2,13 @@
 # Parses id3v2.2 and id2v2.3 tags
 # By: Alexander Addy (ala47) and
 
+# use these variables instead of the strings so that errors are easier to find
+# and a spelling error doesn't happen
+# DO NOT CHANGE THEM!
+V2 = "v22"
+V3 = "v23"
+V4 = "v24"
+
 def id3Ver(mp3_header):
 	pass
 	
@@ -20,16 +27,31 @@ def frameSplit(mp3):
 	pass
 
 # assumes that the file is of type id3v2.2 or id3v2.3
-class id3Parsed(mp3_file):
-	"Given a mp3 file name, this will parse the id3 tag."""
+class id3Parsed():
+	"""Given a mp3 file name, this will parse the id3 tag."""
 	# put all the attributes the user might want into a main dictionary
-	def __init__(self):
+	def __init__(self, mp3_file):
 		self.attrs = {}
 		
 		# open the file
-		# get the the file cursor to the proper place (just after "id3" | "ID3")
+		f = open(mp3_file, "rb")
+		
+		# get the the file cursor to the proper place (just after "ID3")
+		for b in f:
+			if(b == 'I'):
+				temp = f.read(2)
+				if(temp == 'D3'): break
 
 		self.ver = id3Ver(mp3_header)
 
 		# get the encoding string
 		self.encoding = whichEncoding(encoding_byte)
+		
+		# close the file
+		f.close()
+		
+
+if __name__ == "__main__":
+	mp3_file = open("Kalimba.mp3")
+	
+	print(str(mp3_file.read(20)))
