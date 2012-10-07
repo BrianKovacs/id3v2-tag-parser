9617cb8787575cf0658eb8dd3d54f5093339c64e
Alex Addy <alex.addy@gmail.com>
2012-10-07 19:10:13 -0400

    PROGRESS!
    Did more work on the init function. The class is growing rather quickly,
    considering breaking it up.

--- id3_parser.py
+++ id3_parser.py
@@ -9,9 +9,7 @@ V2 = "v22"
 V3 = "v23"
 V4 = "v24"
 
-def id3Ver(mp3_header):
-	pass
-	
+
 def whichEncoding(encoding_byte):
 	"""Returns the encoding type as a string.
 		$00 - ISO-8859-1 (ASCII)
@@ -41,17 +39,66 @@ class id3Parsed():
 			if(b == 'I'):
 				temp = f.read(2)
 				if(temp == 'D3'): break
+				else: f.seek(-2, 1)
 
-		self.ver = id3Ver(mp3_header)
+		# determine if the end of the file was reached
+		if(f.read(1) == ''):# does this work? need to check
+			f.close()
+			raise TypeError("file does not contain id3v2 tag information")
+		else: f.seek(-1, 1)
 
-		# get the encoding string
-		self.encoding = whichEncoding(encoding_byte)
-		
+		self.ver, self.revision = self.id3Ver(f)
+
+		# determine what the version is and react accordingly
+		self.flags = {}
+		if(self.ver == 2):
+			# the version is 2.2
+			# to be implemented
+			pass
+		elif(self.ver == 3):
+			# the version is 2.3
+			# to be implemented
+			pass
+		elif(self.ver == 4):
+			# the version is 2.4
+			# to be implemented
+			pass
+		else:
+			# to be implemented
+			# raise some sort of error?
+			pass
+			
 		# close the file
 		f.close()
 		
+	def id3Ver(self, f):
+		ver = f.read(1)
+		rev = f.read(1)
+		return ver, rev
+		
+	def flags2_2(self, flagbyte):
+		if(flagbyte and 0b10000000): self.flags["synchro"] = True
+		else: self.flags["synchro"] = False
+		
+		if(flagbyte and 0b01000000): self.flags["extended"] = True
+		else: self.flags["extended"] = False
+		
+		if(flagbyte and 0b00100000): self.flags["experimental"] = True
+		else: self.flags["experimental"] = False
+		
+	def flags2_3(self, flagbyte): # to be implemented
+		pass
+		
+	def flags2_4(self, flagbyte): # to be implemented
+		pass
+		
+	def tagSize(f): # to be implemented
+		# 4 bytes where the MSB is zero and ignored
+		# 4 * 0b0xxxxxxx, giving a total of 28 bits
+		pass
 
 if __name__ == "__main__":
-	mp3_file = open("Kalimba.mp3")
-	
-	print(str(mp3_file.read(20)))
+
+	# place holder to test the file to be opened
+	mp3_file = open("Kalimba.mp3", 'rb')
+	print(mp3_file.read(5))
